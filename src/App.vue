dd4f175d547d763ab1a1d4311766b611472c92a8
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-06-22 22:05:37 +0900

    CompositionAPIのお試し

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <InputText/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import InputText from './components/InputText.vue'
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    InputText
   }
 });
 </script>
---
+++ src/components/InputText.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>{{ text.value }}</h2>
+        <p>{{ messageTwo }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { defineComponent, reactive, ref } from "@vue/composition-api";
+export default defineComponent({
+    setup() {
+        // ここにリアクティブなデータ、関数を定義
+        const text = ref({
+            value:"お試しサンプル"
+        });
+        const messageTwo = ref<string>("CompositionAPI分からん……");
+        return{
+            text,
+            messageTwo
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
