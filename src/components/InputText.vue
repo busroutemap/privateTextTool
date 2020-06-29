4c2d88bfc943dd0d00febd3158136b7f14eb044b
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-06-29 23:04:43 +0900

    デザインの変更

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,23 +1,44 @@
 <template>
-    <div>
+    <div
+    class="it"
+    >
         <h2
         v-text="test.value"
         />
-        <p>{{ messageTwo }}</p>
-        <div>
-            <form>
+        <p
+        v-text="messageTwo"
+        />
+        <div
+        class="grid"
+        >
+            <form
+            class="f"
+            >
+                <label
+                v-text="test.before"
+                />
                 <textarea
                 v-model="input.text"
+                placeholder="テキストを入力してください"
                 :cols="setting.tacols"
                 :rows="setting.tarows"
                 />
             </form>
-            <form>
+            <p
+            class="arrow"
+            >👉<br>置換</p>
+            <form
+            class="f"
+            >
+                <label
+                v-text="test.after"
+                />
                 <textarea
                 v-model="output.text"
                 :cols="setting.tacols"
                 :rows="setting.tarows"
-                readonly
+                placeholder="置換後のテキストが表示されます"
+                class="gray"
                 />
             </form>
         </div>
@@ -43,7 +64,9 @@ export default defineComponent({
     setup(props:Props) {
         // ここにリアクティブなデータ、関数を定義
         const test = ref({
-            value:"compositionAPIのテスト"
+            value:"compositionAPIのテスト",
+            before:"置換前",
+            after:"置換後(読み取り専用)"
         });
         const input = reactive({
             text:"",
@@ -70,5 +93,25 @@ export default defineComponent({
 </script>
 
 <style>
-
+.it{
+    border:4px dotted black;
+}
+.grid{
+    display: grid;
+    grid-template-columns: 8fr 1fr 8fr;
+}
+.f{
+    display: grid;
+    grid-template-rows:1fr 9fr;
+    padding:0% 5% 5%;
+}
+.arrow{
+    padding:50% 0% 50% 0%;
+    font-weight: bold;
+    font-size: 2rem;
+}
+.gray{
+    background-color: rgba(128, 128, 128, 0.25);
+    font-weight: bold;
+}
 </style>
