1717c094f46a44b1b7e83b262615fd70328ef13f
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-06-29 23:04:36 +0900

    半角スペースのタブ化機能実装、配列の分割・結合実装

--- src/components/convert.ts
+++ src/components/convert.ts
@@ -1,11 +1,39 @@
 
 /**
+ * @summary 入力文字列を改行コードでばらす
+ * @param longText 長い文字列
+ * @returns 1行ごとに分かれた文字列
+ */
+const nsplit = (longText: string) => {
+    return longText.split("\n");
+};
+
+/**
+ * @summary 入力文字列配列を改行コードで連結する
+ * @param lines 1行ごとの文字列配列
+ * @returns 1行にまとめられた文字列
+ */
+const concatLines = (lines: string[]) => {
+    let longText = "";
+    lines.forEach(line => {
+        longText += line + "\n";
+    });
+    return longText;
+};
+
+
+
+/**
  * @summary ルールに従いテキストを変換する
  * @param longText
  * @returns 置換後のテキスト
  */
 const convert = (longText: string) => {
-    const newText = longText.replace("    ", "\t");
+    const lines = nsplit(longText);
+    const newLines = lines.map(line => {
+        return line.replace("    ", "\t");
+    });
+    const newText = concatLines(newLines);
     return newText;
 };
 
