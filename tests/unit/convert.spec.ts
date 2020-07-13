bdea558ee12840d83774053f878bdd7f8cb726ac
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-07-13 22:21:50 +0900

    ユニットテストのサンプル習得

---
+++ tests/unit/convert.spec.ts
@@ -0,0 +1,11 @@
+
+// サンプル
+
+import { expect } from 'chai'
+
+describe('simplest test:', () => {
+    it('1 + 1 should be 2', () => {
+        const a = 1 + 1;
+        expect(a).to.equal(2)
+    })
+})
