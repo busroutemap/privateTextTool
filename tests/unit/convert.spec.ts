6528704f62407c5688711df7738d022f63547f1e
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-07-18 22:27:37 +0900

    convert.tsのテストを実装

--- tests/unit/convert.spec.ts
+++ tests/unit/convert.spec.ts
@@ -1,11 +1,53 @@
 
 // サンプル
-
+import { convert } from '@/components/convert'
 import { expect } from 'chai'
 
-describe('simplest test:', () => {
-    it('1 + 1 should be 2', () => {
-        const a = 1 + 1;
-        expect(a).to.equal(2)
+const inputText = `
+# [TAG]TEXT H1
+- リスト1
+- リスト2
+    1. 項目1
+    1. 項目2
+    1. 項目3
+## TEXT H2
+- これはH2のテキストです。
+    - 詳細1
+    - 詳細2
+
+### TEXT H3
+- \`詳細3\`
+
+`
+
+const outputText = `
+
+●TAG●
+TEXT H1
+・	リスト1
+・	リスト2
+	1. 項目1
+	1. 項目2
+	1. 項目3
+
+	TEXT H2
+	・	これはH2のテキストです。
+		・	詳細1
+		・	詳細2
+	
+
+		TEXT H3
+		・	\`詳細3\`
+		
+		
+		
+		
+		
+`
+
+describe('convert.ts:', () => {
+    it('メソッド・convert', () => {
+        const result = convert(inputText);
+        expect(result).to.equal(outputText)
     })
 })
